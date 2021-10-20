324784781ba7e9d34b8be44cdc91a84ffd09d96f
prashant-ux <prashant.tiwari_ec16@gla.ac.in>
2021-10-20 18:36:15 +0530

    first model in django

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+from typing import overload
+
+
+def hi(name):
+   print('hi ' + name + ' !')
+girls = ['Rachel', 'Monica', 'klf']
+for name in girls:
+    hi(name)
+    print('Next girl')
+       
+
